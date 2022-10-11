b43a593770c46c19e28c4cf6e9088fb9afa84afe
Oba <whatobathinks@gmail.com>
2022-10-11 02:06:09 -0500

    Additiona blog post layout changes

--- src/js/custom-javascript.js
+++ src/js/custom-javascript.js
@@ -4,10 +4,10 @@ const navButton = document.querySelector('#navbarButton');
 const mobileMenu = document.querySelector('#mobileMenu');
 
 navButton.addEventListener('click', (e) => {
-   e.stopPropagation();
-   if (mobileMenu.classList.contains('active')) {
-      mobileMenu.classList.remove('active');
-   } else mobileMenu.classList.add('active');
+    e.stopPropagation();
+    if (mobileMenu.classList.contains('active')) {
+        mobileMenu.classList.remove('active');
+    } else mobileMenu.classList.add('active');
 });
 
 // Live search function
@@ -17,34 +17,34 @@ const loader = document.querySelector('#loader');
 let timeout;
 
 function displayPosts(data) {
-   if (data.length > 0) {
-      data.map((post) => {
-         searchResults.innerHTML += `<a href="${post.link}" class="posts__search-results__link">${post.title.rendered}</a>`;
-      });
-   } else {
-      searchResults.innerHTML = `<span class="posts__search-results__empty">No search results</span>`;
-   }
+    if (data.length > 0) {
+        data.map((post) => {
+            searchResults.innerHTML += `<a href="${post.link}" class="posts__search-results__link">${post.title.rendered}</a>`;
+        });
+    } else {
+        searchResults.innerHTML = `<span class="posts__search-results__empty">No search results</span>`;
+    }
 }
 
 function getPosts() {
-   if (searchInput.value.length !== 0) {
-      fetch(`${blogData.rootUrl}/wp-json/wp/v2/posts?search=${searchInput.value}`)
-         .then((res) => res.json())
-         .then((data) => displayPosts(data))
-         .catch((e) => console.log('JS Error:', e));
-   }
-   return;
+    if (searchInput.value.length !== 0) {
+        fetch(`${blogData.rootUrl}/wp-json/wp/v2/posts?search=${searchInput.value}`)
+            .then((res) => res.json())
+            .then((data) => displayPosts(data))
+            .catch((e) => console.log('JS Error:', e));
+    }
+    return;
 }
 
 searchInput.addEventListener('input', () => {
-   loader.classList.add('show');
+    loader.classList.add('show');
 
-   clearTimeout(timeout);
-   timeout = setTimeout(() => {
-      loader.classList.remove('show');
-      searchResults.innerHTML = '';
-      getPosts();
-   }, 1000);
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+        loader.classList.remove('show');
+        searchResults.innerHTML = '';
+        getPosts();
+    }, 1000);
 });
 
 //
